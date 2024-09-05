748556f95f51b9969fb5fe46ec5eea9a4e446dec
Federico Brigante <me@fregante.com>
2024-09-05 13:59:41 +0700

    Tighten tests and make them readable (#19)

--- test.js
+++ test.js
@@ -1,67 +1,45 @@
+/* eslint-disable no-sparse-arrays -- Clearer visual comparison */
+
 import test from 'ava';
 import issueRegex from './index.js';
 
-const matches = [
-	'#1',
-	'#3223',
-	'sindresorhus/dofle#33',
-	'foo-bar/unicorn.rainbow#21',
-	'foo/a#1',
-	'a/foo#1',
-	'thisorganisationnameislongbutokxxxxxxxx/foo#123',
-	'foo/thisrepositorynameislongbutokxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx#123',
-	'#1111111111',
-	'foo/longbutokissuenumber#1111111111',
-	'foo/-#123',
-	'foo/-bar#123',
-	'foo/bar-#123',
-	'foo/foo-bar#123',
-	'foo/.bar#123',
-	'foo/..bar#123',
-	'foo/...#123',
-	'foo/_#123',
-	'foo/0#123',
-	'0/bar#123',
-	'1/1#1',
-	'Foo/Bar#1',
-];
+const matches = test.macro({
+	exec(t, input, expectedArray, description) {
+		// Verify test input
+		t.true(Array.isArray(expectedArray), 'Expected array, got', typeof expectedArray);
+		t.is(expectedArray.length, 3, 'Expected array length 3, got', expectedArray.length);
 
-const nonMatches = [
-	'#',
-	'#0',
-	'#x',
-	'123',
-	'sindresorhus/dofle',
-	'sindresorhus/dofle#',
-	'sindresorhus/dofle#0',
-	'dofle#33',
-	'#123hashtag',
+		// Baseline match
+		const match = issueRegex().exec(input);
+		t.truthy(match, `should match but doesn't${description ? `: ${description}` : ''}`);
 
-	// GitHub organization names can't be longer than 39 characters
-	// as of March 2022.
-	// Source: GitHub shows an error message when trying to create
-	// an organization with a longer name. See issue #11.
-	'thisorganisationnameistoolongxxxxxxxxxxx/foo#123',
+		// Ensure that both the named capture groups and the index-based matches are correct
+		t.deepEqual(match.slice(1), expectedArray, description);
+		t.deepEqual(match.groups, {
+			organization: expectedArray[0],
+			repository: expectedArray[1],
+			issueNumber: expectedArray[2],
+		}, description);
 
-	// GitHub repository names can't be longer than 100 characters
-	// as of March 2022.
-	// Source: The text box on the repository creation page has a
-	// maxLength of 100. See issue #11.
-	'foo/thisrepositorynameistoolongxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx#123',
+		// Verify the match is unchanged when the reference appears in the middle of a string
+		t.deepEqual(issueRegex().exec(`Very #middle ${input} much/tricky#`).groups, match.groups, description);
+	},
+	title(_, input) {
+		return `should match ${input}`;
+	},
+});
 
-	// A GitHub issue number shouldn't have an infinite number of digits. Limit to
-	// 10B issues (10^10-1).
-	'#11111111111',
-	'foo/thisissuenumberistoolong#11111111111',
+const noMatch = test.macro({
+	exec(t, input, description) {
+		t.falsy(issueRegex().exec(input), description);
+		t.falsy(issueRegex().exec(`Very #middle ${input} much/tricky#`), description);
+	},
+	title(_, input) {
+		return `should not match ${input}`;
+	},
+});
 
-	'foo_bar/bar',
-	'-foo/bar',
-	'foo-/bar',
-	'foo.bar/bar',
-	'foo/.',
-	'foo/..',
-];
-
+// Ensure that multiple patterns can be matched at once
 test('baseline', t => {
 	t.deepEqual('Fixes #143 and avajs/ava#1023'.match(issueRegex()), [
 		'#143',
@@ -69,103 +47,213 @@ test('baseline', t => {
 	]);
 });
 
-test('should match', t => {
-	for (const x of matches) {
-		t.is((issueRegex().exec(`foo ${x} bar`) || [])[0], x);
-	}
-});
+// Test cases for matching patterns
+test(
+	matches,
+	'#1',
+	[,, '1'],
+);
+test(
+	matches,
+	'#3223',
+	[,, '3223'],
+);
+test(
+	matches,
+	'sindresorhus/dofle#33',
+	['sindresorhus', 'dofle', '33'],
+);
+test(
+	matches,
+	'foo-bar/unicorn.rainbow#21',
+	['foo-bar', 'unicorn.rainbow', '21'],
+);
+test(
+	matches,
+	'foo/a#1',
+	['foo', 'a', '1'],
+);
+test(
+	matches,
+	'a/foo#1',
+	['a', 'foo', '1'],
+);
+test(
+	matches,
+	'thisorganisationnameislongbutokxxxxxxxx/foo#123',
+	['thisorganisationnameislongbutokxxxxxxxx', 'foo', '123'],
+);
+test(
+	matches,
+	'foo/thisrepositorynameislongbutokxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx#123',
+	['foo', 'thisrepositorynameislongbutokxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', '123'],
+);
+test(
+	matches,
+	'#1111111111',
+	[,, '1111111111'],
+);
+test(
+	matches,
+	'foo/longbutokissuenumber#1111111111',
+	['foo', 'longbutokissuenumber', '1111111111'],
+);
+test(
+	matches,
+	'foo/-#123',
+	['foo', '-', '123'],
+);
+test(
+	matches,
+	'foo/-bar#123',
+	['foo', '-bar', '123'],
+);
+test(
+	matches,
+	'foo/bar-#123',
+	['foo', 'bar-', '123'],
+);
+test(
+	matches,
+	'foo/foo-bar#123',
+	['foo', 'foo-bar', '123'],
+);
+test(
+	matches,
+	'foo/.bar#123',
+	['foo', '.bar', '123'],
+);
+test(
+	matches,
+	'foo/..bar#123',
+	['foo', '..bar', '123'],
+);
+test(
+	matches,
+	'foo/...#123',
+	['foo', '...', '123'],
+);
+test(
+	matches,
+	'foo/_#123',
+	['foo', '_', '123'],
+);
+test(
+	matches,
+	'foo/0#123',
+	['foo', '0', '123'],
+);
+test(
+	matches,
+	'0/bar#123',
+	['0', 'bar', '123'],
+);
+test(
+	matches,
+	'1/1#1',
+	['1', '1', '1'],
+);
+test(
+	matches,
+	'Foo/Bar#1',
+	['Foo', 'Bar', '1'],
+);
+test(
+	matches,
+	'#123',
+	[,, '123'],
+);
+test(
+	matches,
+	'#666',
+	[,, '666'],
+);
+test(
+	matches,
+	'another/repo#123',
+	['another', 'repo', '123'],
+);
+test(
+	matches,
+	'ano-ther.999/re_po#123',
+	['999', 're_po', '123'],
+	'Organization names cannot contain dots',
+);
+test(
+	matches,
+	'(#123)',
+	[,, '123'],
+);
+test(
+	matches,
+	'[#123]',
+	[,, '123'],
+);
+test(
+	matches,
+	'<another/repo#123>',
+	['another', 'repo', '123'],
+);
+test(
+	matches,
+	'this/is/ok/repo#444',
+	['ok', 'repo', '444'],
+	'GitHub repository names can\'t contain a slash',
+);
+test(
+	matches,
+	'this/is.ok/repo#444',
+	['ok', 'repo', '444'],
+	'GitHub repository names can\'t contain a dot',
+);
+test(
+	matches,
+	'-ok/repo#444',
+	['ok', 'repo', '444'],
+	'GitHub repository names can\'t start with a dash',
+);
+test(
+	matches,
+	'foo/bar.#123',
+	['foo', 'bar.', '123'],
+	'GitHub repository names can end with a dot',
+);
+test(
+	matches,
+	'#999',
+	[,, '999'],
+);
+test.failing(
+	matches,
+	'forkuser#123',
+	['forkuser',, '123'],
+);
 
-test('should not match', t => {
-	for (const x of nonMatches) {
-		t.is(issueRegex().exec(`foo ${x} bar`), null);
-	}
-});
+// Test cases for invalid patterns
+test(noMatch, '#');
+test(noMatch, '#0');
+test(noMatch, '#x');
+test(noMatch, '123');
+test(noMatch, 'sindresorhus/dofle');
+test(noMatch, 'sindresorhus/dofle#');
+test(noMatch, 'sindresorhus/dofle#0');
+test(noMatch, 'dofle#33');
+test(noMatch, '#123hashtag');
 
-test('extract from string', t => {
-	// https://regex101.com/r/SQrOlx/12
-	const actual = `#123
+// Source: As of March 2022 GitHub shows an error message when trying to create an organization with a longer name. See issue #11.
+test(noMatch, 'thisorganisationnameistoolongxxxxxxxxxxx/foo#123', 'GitHub organization names can\'t be longer than 39 characters');
 
-	Should match:
+// Source: As of March 2022 the text box on the repository creation page has a maxLength of 100. See issue #11.
+test(noMatch, 'foo/thisrepositorynameistoolongxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx#123', 'GitHub repository names can\'t be longer than 100 characters');
 
-	- Plain issue: #666
-	- From another repository: another/repo#123
-	- Crazy formatting: ano-ther.999/re_po#123
-	- In brackets: (#123), [#123], <another/repo#123>
-	- this/is/ok/repo#444
-	- this/is.ok/repo#444
-	- -ok/repo#444
-	- foo/-bar#123
-	- foo/-#123
-	- foo/.bar#123
-	- foo/bar.#123
-	- foo/...#123
-	- foo_bar/bar#123
+test(noMatch, '#11111111111', 'A GitHub issue number shouldn\'t have an infinite number of digits. Limit to 10B issues (10^10-1).');
+test(noMatch, 'foo/thisissuenumberistoolong#11111111111');
 
-	Should NOT match:
+test(noMatch, 'foo_bar/bar');
+test(noMatch, '-foo/bar');
+test(noMatch, 'foo-/bar');
+test(noMatch, 'foo.bar/bar');
 
-	- #0
-	- another/repo#0
-	- nonrepo#123
-	- user_repo#123
-	- #123hashtag
-	- foo/.#111
-	- foo/..#222
-  - _foo/bar#123
-
-	#999`;
-
-	const expected = [
-		'#123',
-		'#666',
-		'another/repo#123',
-		'999/re_po#123',
-		'#123',
-		'#123',
-		'another/repo#123',
-		'ok/repo#444',
-		'ok/repo#444',
-		'ok/repo#444',
-		'foo/-bar#123',
-		'foo/-#123',
-		'foo/.bar#123',
-		'foo/bar.#123',
-		'foo/...#123',
-		'#999',
-	];
-
-	t.deepEqual(actual.match(issueRegex()), expected);
-});
-
-test('capturing groups: all', t => {
-	const match = issueRegex().exec('foo/bar#123');
-	t.is(match[1], 'foo');
-	t.is(match[2], 'bar');
-	t.is(match[3], '123');
-
-	t.is(match.groups.organization, 'foo');
-	t.is(match.groups.repository, 'bar');
-	t.is(match.groups.issueNumber, '123');
-});
-
-test('capturing groups: issue only', t => {
-	const match = issueRegex().exec('#123');
-	t.is(match[1], undefined);
-	t.is(match[2], undefined);
-	t.is(match[3], '123');
-
-	t.is(match.groups.organization, undefined);
-	t.is(match.groups.repository, undefined);
-	t.is(match.groups.issueNumber, '123');
-});
-
-// TODO: Tracked in https://github.com/sindresorhus/issue-regex/issues/17
-test.failing('capturing groups: no-repository', t => {
-	const match = issueRegex().exec('forkuser#123');
-	t.is(match[1], 'forkuser');
-	t.is(match[2], undefined);
-	t.is(match[3], '123');
-
-	t.is(match.groups.organization, 'forkuser');
-	t.is(match.groups.repository, undefined);
-	t.is(match.groups.issueNumber, '123');
-});
+// Reserved names
+test(noMatch, 'foo/.');
+test(noMatch, 'foo/..');
